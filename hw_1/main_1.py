537c419f2059b2eb425a3dda2dcf3c4a70ca8968
krishtalevia <krishtalevia@gmail.com>
2024-05-05 15:44:00 +0300

    dev class animal methods

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,11 +1,18 @@
+# №1
+
 class Animal:
-    def __init__(self, name: str, type: str, age: int):
+    def __init__(self, name: str, type: str, age: int, sound: str):
         self.name = name
         self.type = type
         self.age = age
+        self.sound = sound
 
     def print_sound(self):
-        pass
+        print(f'{self.name} издает звук "{self.sound}"')
 
     def print_info(self):
-        pass
+        print(f'Имя: {self.name}')
+        print(f'Вид: {self.type}')
+        print(f'Возраст: {self.age}')
+        print(f'Издаваемый звук: {self.sound}')
+        
