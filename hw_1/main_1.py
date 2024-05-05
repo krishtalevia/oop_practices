0059ad4adb783b6ab652c3105dca82be4c344c6b
krishtalevia <krishtalevia@gmail.com>
2024-05-05 15:35:12 +0300

    create methods print sound/info in animal class

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,3 +1,11 @@
 class Animal:
     def __init__(self, name: str, type: str, age: int):
+        self.name = name
+        self.type = type
+        self.age = age
+
+    def print_sound(self):
+        pass
+
+    def print_info(self):
         pass
