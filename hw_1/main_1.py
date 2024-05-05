7b342f7cf4d59c9d69109b3ac415ae733a2d7d63
krishtalevia <krishtalevia@gmail.com>
2024-05-05 15:56:27 +0300

    create class book header

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,5 +1,4 @@
 # №1
-
 class Animal:
 
     def __init__(self, name: str, type: str, age: int, sound: str):
@@ -18,13 +17,20 @@ class Animal:
         print(f'Возраст: {self.age}')
         print(f'Издаваемый звук: {self.sound}')
 
-betty_the_cow = Animal(name='Betty', type='Cow', age=1, sound='Moo')
 
 class Program:
 
     @staticmethod
     def main():
+        betty_the_cow = Animal(name='Betty', type='Cow', age=1, sound='Moo')
+
         betty_the_cow.print_sound()
         betty_the_cow.print_info()
 
 Program.main()
+
+# №2
+class Book:
+
+    def __init__(self, name: str, author: str, page_qty: int):
+        pass
