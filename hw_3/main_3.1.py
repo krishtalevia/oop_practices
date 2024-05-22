8ebd84d701c1889b8db60562777f05fda6c34b64
krishtalevia <krishtalevia@gmail.com>
2024-05-23 00:10:43 +0300

    add getters

--- hw_3/main_3.1.py
+++ hw_3/main_3.1.py
@@ -3,13 +3,27 @@ from __future__ import annotations
 # 1.1
 class Wizard:
 
-    def __init__(self, name: str, faculty: str, level: int, spells: list, student_status: bool):
+    def __init__(self, name: str, house: str, magic_level: int, spells: list, status: bool):
         self.__name = name
-        self.__faculty = faculty
-        self.__level = level
+        self.__house = house
+        self.__magic_level = magic_level
         if spells == None:
             self.__spells = []
         else:
             self.__spells = spells
-        self.__student_status = student_status
+        self.__status = status
 
+    def get_name(self):
+        return self.__name
+
+    def get_house(self):
+        return self.__house
+
+    def get_magic_level(self):
+        return self.__magic_level
+
+    def get_spells(self):
+        return self.__spells
+
+    def get_status(self):
+        return self.__status
