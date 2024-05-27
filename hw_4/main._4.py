fb3e37fbe9ef862a9c909d84eccefe661caeab11
krishtalevia <krishtalevia@gmail.com>
2024-05-27 16:30:25 +0300

    add class Student

---
+++ hw_4/main._4.py
@@ -0,0 +1,19 @@
+class Student:
+
+    def __init__(self, first_name: str, last_name: str, age: int, avg_mark: int):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
+        self.__avg_mark = avg_mark
+
+    def get_first_name(self):
+        pass
+    
+    def get_last_name(self):
+        pass
+
+    def get_age(self):
+        pass
+
+    def get_avg_mark(self):
+        pass
