f44020524d67d8c4675e10e32e39915530328ce3
krishtalevia <krishtalevia@gmail.com>
2024-05-31 12:57:09 +0300

    create class Employee

--- hw_4/task_2/main._4.py
+++ hw_4/task_2/main._4.py
@@ -150,4 +150,62 @@ class Book:
                 f'Жанры: {self.get_genres()}')
 
 class Employee:
-    pass
+
+    def __init__(self, name: str, position: str, id: int, contact_info: list[ContactInfo] = None):
+        self.__name = name
+        self.__position = position
+        self.__id = id
+        if contact_info == None:
+            self.__contact_info = []
+        else:
+            self.__contact_info = contact_info
+
+    def get_name(self):
+        return self.__name
+
+    def get_position(self):
+        return self.__position
+
+    def get_id(self):
+        return self.__id
+
+    def get_contact_info(self):
+        contact_info = '|'
+        for i in self.__contact_info:
+            i = i.get_value()
+            contact_info += f' {str(i)} |'
+        return contact_info
+
+    def set_name(self, name: str):
+        if not isinstance(name, str): raise TypeError('Неподходящий тип данных!')
+
+        self.__name = name
+
+    def set_position(self, position: str):
+        if not isinstance(position, str): raise TypeError('Неподходящий тип данных!')
+
+        self.__position = position
+
+    def set_id(self, id: int):
+        if not isinstance(id, str): raise TypeError('Неподходящий тип данных!')
+
+        self.__id = id
+
+    def add_contact_info(self, contact_info: ContactInfo):
+        if not isinstance(contact_info, ContactInfo): raise TypeError('Неподходящий тип данных!')
+
+        self.__contact_info.append(contact_info)
+
+    def remove_contact_info(self, contact_info: ContactInfo):
+        if not isinstance(contact_info, ContactInfo): raise TypeError('Неподходящий тип данных!')
+
+        for i in range(0, len(self.__contact_info), 1):
+            if self.__contact_info[i] == contact_info:
+                del self.__contact_info[i]
+                break
+
+    def __str__(self):
+        return (f'Имя: {self.__name}'
+                f'Должность: {self.__position}'
+                f'ID: {self.__id()}'
+                f'Контакт. информация: {self.get_contact_info()}')
