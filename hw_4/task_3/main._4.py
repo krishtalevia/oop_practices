e696606270394687c4e63acd56ae405515827af4
krishtalevia <krishtalevia@gmail.com>
2024-05-31 16:33:23 +0300

    create class Program, task 3

--- hw_4/task_3/main._4.py
+++ hw_4/task_3/main._4.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+
 # 1
 class Car:
 
@@ -42,6 +43,7 @@ class Car:
                 f'Цена: {self.__price}\n'
                 f'Статус: {self.__status}\n')
 
+
 class Salesperson:
 
     def __init__(self, name: str, experience: int, sold_cars: list[Car]):
@@ -71,6 +73,7 @@ class Salesperson:
         return (f'Имя: {self.__name}\n'
                 f'Опыт: {self.__experience}\n'
                 f'Проданные авто: {self.get_sold_cars()}\n')
+
 
 class Customer:
 
@@ -148,6 +151,26 @@ class Dealership:
 
         self.__cars.append(car)
 
+    def remove_car(self, car: Car):
+        if not isinstance(car, Car): raise TypeError('Неподходящий тип данных!')
+
+        for i in range(0, len(self.__cars), 1):
+            if self.__cars[i] == car:
+                del self.__cars[i]
+                break
+
+    def sell_car(self, car: Car, salesperson: Salesperson, customer: Customer):
+        if not isinstance(car, Car) \
+                or not isinstance(salesperson, Salesperson) \
+                or not isinstance(customer, Customer): raise TypeError('Неподходящий тип данных!')
+
+        if car in self.__cars:
+            car.set_status('Продано')
+            salesperson.add_sold_car(car)
+            customer.add_car(car)
+
+            self.remove_car(car)
+
     def add_salesperson(self, salesperson: Salesperson):
         if not isinstance(salesperson, Salesperson): raise TypeError('Неподходящий тип данных!')
 
@@ -167,24 +190,45 @@ class Dealership:
         self.__customers.append(customer)
 
     def set_car_price(self, car: Car, price: float):
-        if not isinstance(car, Customer) \
+        if not isinstance(car, Car) \
                 or not isinstance(price, float): raise TypeError('Неподходящий тип данных!')
 
         if not price > 0: raise ValueError('Цена не может быть меньше 0')
 
-        self.__cars[car].set_price(price)
-
-    def set_car_status(self, car: Car, status: str):
-        if not isinstance(car, Customer) \
-                or not isinstance(status, str): raise TypeError('Неподходящий тип данных!')
-
-        self.__cars[car].set_status(status)
+        car.set_price(price)
 
     def __str__(self):
         return (f'Авто: {self.get_cars()}\n'
                 f'Продавцы: {self.get_salespersons()}\n'
                 f'Клиенты: {self.get_customers()}\n')
 
+class Program:
+
+    @staticmethod
+    def main():
+        first_car = Car('ВАЗ', '01', 1400, 1000, 'Продано')
+        second_car = Car('АВЗ', '02', 1401, 2000, 'В наличии')
+        third_car = Car('ЗАВ', '03', 1402, 3000, 'Ожидается')
+
+        oleg_the_salesperson = Salesperson('Олег', 2, [first_car])
+
+        igor_the_customer = Customer('Игорь', 89123456789, 'igor@email.com', [first_car])
+
+        first_dealership = Dealership([second_car, third_car], [oleg_the_salesperson],
+                                      [igor_the_customer])
+
+        # print(first_car)
+        # print(oleg_the_salesperson)
+        # print(first_dealership)
+        #
+        # first_dealership.sell_car(third_car, oleg_the_salesperson, igor_the_customer)
+        #
+        # print(igor_the_customer)
+        #
+        # print(first_dealership)
+
+        first_dealership.set_car_price(first_car, 5000.00)
+        print(first_car)
 
 
-
+Program.main()
