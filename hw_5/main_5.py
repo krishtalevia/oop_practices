7baf26c4e9899e7286c9a175f19e8fffd4029fbd
krishtalevia <krishtalevia@gmail.com>
2024-06-02 18:32:29 +0300

    create class Animal

---
+++ hw_5/main_5.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+class Animal:
+
+    def __init__(self, name: str, species: str):
+        self.__name = name
+        self.__species = species
+
+    def make_sound(self):
+        pass
