b7a9611d2bcaef4fbb83a20016a98d4b2735414d
krishtalevia <krishtalevia@gmail.com>
2024-06-02 18:38:17 +0300

    add classes Dog/Cat/Bird

--- hw_5/main_5.py
+++ hw_5/main_5.py
@@ -7,4 +7,31 @@ class Animal:
         self.__species = species
 
     def make_sound(self):
-        pass
+        print('Animal sound')
+
+class Dog(Animal):
+
+    def make_sound(self):
+        print('Woof')
+
+class Cat(Animal):
+
+    def make_sound(self):
+        print('Meow')
+
+class Bird(Animal):
+
+    def make_sound(self):
+        print('Chirp')
+
+class Program:
+
+    @staticmethod
+    def main():
+
+        dog = Dog('Mel Gibson', 'Bulldog')
+        dog.make_sound()
+        bird = Bird('Adam Sandler', 'Pigeon')
+        bird.make_sound()
+
+Program.main()
